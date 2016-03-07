7137ca297f32f0ec4103462cc847483d007e66d9
Marinlah <Hugo.Marin-Lamellet@etu.univ-savoie.fr>
2016-03-07 17:42:57 +0100

    Début listView + 2 trois trucs

--- app/src/main/java/com/example/srava/testgithub2/TestGithub2.java
+++ app/src/main/java/com/example/srava/testgithub2/TestGithub2.java
@@ -1,17 +1,46 @@
 package com.example.srava.testgithub2;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 
-public class TestGithub2 extends ActionBarActivity {
+public class TestGithub2 extends Activity implements OnClickListener{
 
     @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_test_github2);
+
+        Button button1=(Button)findViewById(R.id.btnTest); // Récupération de l'instance bouton 1
+        button1.setOnClickListener((OnClickListener) this); // Positionnons un listener sur ce bouton
+
+
+        String[] mStrings = {
+                "AAAAAAAA", "BBBBBBBB", "CCCCCCCC", "DDDDDDDD", "EEEEEEEE",
+                "FFFFFFFF", "GGGGGGGG", "HHHHHHHH", "IIIIIIII", "JJJJJJJJ",
+                "KKKKKKKK", "LLLLLLLL", "MMMMMMMM", "NNNNNNNN", "OOOOOOOO",
+                "PPPPPPPP", "QQQQQQQQ", "RRRRRRRR", "SSSSSSSS", "TTTTTTTT",
+                "UUUUUUUU", "VVVVVVVV", "WWWWWWWW", "XXXXXXXX", "YYYYYYYY",
+                "ZZZZZZZZ"
+        };
+
+        //Création de l'adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mStrings);
+
+//Récupération du ListView présent dans notre IHM
+        ListView list = (ListView)findViewById(R.id.lvRestos);
+
+//On passe nos données au composant ListView
+        list.setAdapter(adapter);
     }
 
     @Override
@@ -34,5 +63,11 @@ public class TestGithub2 extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public void onClick(View view) {
+        if (view.getId()==R.id.btnTest){ // C'est notre bouton ? oui, alors affichage d'un message
+            Toast.makeText(this,"Bouton 1", Toast.LENGTH_SHORT).show();
+        }
     }
 }
