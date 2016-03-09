8e2b1a59b6e0bb9ce5df233949c70ba198c561c5
PsykoPatato <val_du_74000@hotmail.fr>
2016-03-09 17:45:54 +0100

    LOL C 1P0RTEN

--- app/src/main/java/com/example/srava/testgithub2/MainActivity.java
+++ app/src/main/java/com/example/srava/testgithub2/MainActivity.java
@@ -1,24 +1,35 @@
 package com.example.srava.testgithub2;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.view.View;
 import android.view.View.OnClickListener;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
-public class TestGithub2 extends Activity implements OnClickListener{
+
+public class MainActivity extends Activity implements OnClickListener{
+
+    String JSON_STRING;
 
     @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_test_github2);
+            setContentView(R.layout.activity_main_activity);
 
         Button button1=(Button)findViewById(R.id.btnTest); // Récupération de l'instance bouton 1
         button1.setOnClickListener((OnClickListener) this); // Positionnons un listener sur ce bouton
@@ -46,7 +57,7 @@ public class TestGithub2 extends Activity implements OnClickListener{
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_test_github2, menu);
+        getMenuInflater().inflate(R.menu.menu_main_activity, menu);
         return true;
     }
 
@@ -66,8 +77,62 @@ public class TestGithub2 extends Activity implements OnClickListener{
     }
     @Override
     public void onClick(View view) {
-        if (view.getId()==R.id.btnTest){ // C'est notre bouton ? oui, alors affichage d'un message
+        /*if (view.getId()==R.id.btnTest){ // C'est notre bouton ? oui, alors affichage d'un message
             Toast.makeText(this,"Bouton 1", Toast.LENGTH_SHORT).show();
+
+        }*/
+    }
+
+    public void getJSON(View view){
+        new BackgroundTask().execute();
+    }
+
+    class BackgroundTask extends AsyncTask<Void, Void, String> {
+
+        String json_url;
+        @Override
+        protected void onPreExecute() {
+            json_url = "http://gitdafood.hol.es/index.php";
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            try {
+                URL url = new URL(json_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while((JSON_STRING = bufferedReader.readLine()) != null){
+                    stringBuilder.append(JSON_STRING+"\n");
+                }
+
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+
+                return stringBuilder.toString().trim();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            TextView label1 = (TextView)findViewById(R.id.label1);
+            label1.setText(result);
         }
     }
+
 }
+
