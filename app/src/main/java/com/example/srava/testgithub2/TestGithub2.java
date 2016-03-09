5859ea01f2b297f3bcd4e0ecce8cd6308d5f4a0c
Marinlah <Hugo.Marin-Lamellet@etu.univ-savoie.fr>
2016-03-09 17:44:16 +0100

    TEST DE FDP APRES 35 MIN A CHERCHER CES  BOUTONS DE CONS3

--- app/src/main/java/com/example/srava/testgithub2/TestGithub2.java
+++ app/src/main/java/com/example/srava/testgithub2/TestGithub2.java
@@ -1,10 +1,13 @@
 package com.example.srava.testgithub2;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -15,33 +18,42 @@ import android.view.View.OnClickListener;
 
 public class TestGithub2 extends Activity implements OnClickListener{
 
+    ListView list;
+
     @Override
         protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_test_github2);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_github2);
 
-        Button button1=(Button)findViewById(R.id.btnTest); // Récupération de l'instance bouton 1
+        Button button1 = (Button) findViewById(R.id.btnTest); // Récupération de l'instance bouton 1
         button1.setOnClickListener((OnClickListener) this); // Positionnons un listener sur ce bouton
 
 
         String[] mStrings = {
-                "AAAAAAAA", "BBBBBBBB", "CCCCCCCC", "DDDDDDDD", "EEEEEEEE",
-                "FFFFFFFF", "GGGGGGGG", "HHHHHHHH", "IIIIIIII", "JJJJJJJJ",
-                "KKKKKKKK", "LLLLLLLL", "MMMMMMMM", "NNNNNNNN", "OOOOOOOO",
-                "PPPPPPPP", "QQQQQQQQ", "RRRRRRRR", "SSSSSSSS", "TTTTTTTT",
-                "UUUUUUUU", "VVVVVVVV", "WWWWWWWW", "XXXXXXXX", "YYYYYYYY",
-                "ZZZZZZZZ"
+                "AAAAA", "BBBBB", "CCCCC", "DDDDD", "EEEEE",
+                "FFFFF", "GGGGG"
         };
 
         //Création de l'adapter
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mStrings);
 
 //Récupération du ListView présent dans notre IHM
-        ListView list = (ListView)findViewById(R.id.lvRestos);
+        list = (ListView) findViewById(R.id.lvRestos);
 
 //On passe nos données au composant ListView
         list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView parentView, View childView,
+                                       int position, long id) {
+                Log.d("Debug", "item click");
+                Intent intent= new Intent(TestGithub2.this, Infos.class);
+                startActivity(intent);
+            }
+        });
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -70,4 +82,6 @@ public class TestGithub2 extends Activity implements OnClickListener{
             Toast.makeText(this,"Bouton 1", Toast.LENGTH_SHORT).show();
         }
     }
+
+
 }
